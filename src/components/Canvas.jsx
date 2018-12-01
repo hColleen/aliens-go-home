08d08cfd8b3a88b2a8f7d302a721ac6225c78ada
hColleen <hcolleen@gmail.com>
2018-12-01 06:34:03 -0700

    start creating canvas elements

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sky from './Sky';
+
+const Canvas = () => {
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  return (
+    <svg
+      id="aliens-go-home-canvas"
+      viewBox={viewBox}
+    >
+    <Sky />
+      <circle cx={0} cy={0} r={50} />
+    </svg>
+  );
+};
+
+export default Canvas;
