bb795f91c24f515e5d3d73d19c09eab3f0c8f744
hColleen <hcolleen@gmail.com>
2018-12-01 09:39:07 -0700

    add title/heart/start

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -7,9 +7,13 @@ import CannonPipe from './CannonPipe';
 import CannonBall from './CannonBall'
 import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
+import Heart from './Heart'
+import StartGame from './StartGame'
+import Title from './Title';
 
 const Canvas = (props) => {
-    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+    const gameHeight = 1200;
+    const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
     return (
         <svg
             id="aliens-go-home-canvas"
@@ -29,6 +33,9 @@ const Canvas = (props) => {
             <CurrentScore score={15} />
             <FlyingObject position={{ x: -150, y: -300 }} />
             <FlyingObject position={{ x: 150, y: -300 }} />
+            <Heart position={{ x: -300, y: 35 }} />
+            <StartGame onClick={() => console.log('Aliens, Go Home!')} />
+            <Title />
         </svg>
     );
 };
