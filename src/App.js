3d240303bfe56d13cd39a7a4c663248465371bf1
hColleen <hcolleen@gmail.com>
2018-12-01 07:26:26 -0700

    cannon moving, tracking mouse

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 
-
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+        self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
   render() {
     return (
-      <div className="App">
-        <Canvas />
-      </div>
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => (this.trackMouse(event))}
+      />
     );
   }
 }
 
 App.propTypes = {
-  message: PropTypes.string.isRequired,
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
 };
 
 export default App;
+
+//tutorial from https://auth0.com/blog/developing-games-with-react-redux-and-svg-part-1/ and following parts
