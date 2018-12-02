0277897c57ed0a61c6915cb74040f384eac85c23
hColleen <hcolleen@gmail.com>
2018-12-02 07:01:52 -0700

    score and lives working

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -21,22 +21,39 @@ function moveObjects(state, action) {
     (now - object.createdAt) < 4000
   ));
 
+  const lostLife = state.gameState.flyingObjects.length > flyingObjects.length;
+  let lives = state.gameState.lives;
+  if (lostLife) {
+    lives--;
+  }
+
+  const started = lives > 0;
+  if (!started) {
+    flyingObjects = [];
+    cannonBalls = [];
+    lives = 3;
+  }
+
   const { x, y } = mousePosition;
   const angle = calculateAngle(0, 0, x, y);
 
   const objectsDestroyed = checkCollisions(cannonBalls, flyingObjects);
   const cannonBallsDestroyed = objectsDestroyed.map(object => (object.cannonBallId));
   const flyingDiscsDestroyed = objectsDestroyed.map(object => (object.flyingDiscId));
+  const kills = state.gameState.kills + flyingDiscsDestroyed.length;
 
   cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)));
   flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)));
-  
+
 return {
     ...newState,
     gameState: {
       ...newState.gameState,
       flyingObjects,
       cannonBalls,
+      lives,
+      started,
+      kills,
     },
     angle,
   };
